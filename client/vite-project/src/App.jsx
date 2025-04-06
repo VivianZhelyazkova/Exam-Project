dbccc9f9f59852012deab9e427746317a60698b3
Viviancat666 <vivian9069@gmail.com>
2025-04-06 21:12:26 +0300

    logo added

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import Header from './components/Header'
-import { Routes, Route } from 'react-router'
-import Home from './components/Home'
-import Catalog from './components/Catalog'
-import Login from './components/Login'
-import Register from './components/Register'
+import "./App.css";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router";
+import Home from "./components/Home";
+import Catalog from "./components/Catalog";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  
-
-  return (
-    <>
-     <Header/>
-     <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/catalog' element={<Catalog/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-     </Routes>
-    </>
-  )
+    return (
+        <>
+            <div className="main-container">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- client/vite-project/src/components/Header.jsx
+++ client/vite-project/src/components/Header.jsx
@@ -2,11 +2,13 @@ import { NavLink } from "react-router";
 export default function Header() {
     return (
         <>
+            <div className="header-container">
+            <div className="logo">LOGO</div>
             <NavLink className={({isActive})=>isActive ? "active-link": "link"} to="/">Home</NavLink>
             <NavLink className={({isActive})=>isActive ? "active-link": "link"} to="/catalog">Catalog</NavLink>
             <NavLink className={({isActive})=>isActive ? "active-link": "link"} to="/login">Login</NavLink>
             <NavLink className={({isActive})=>isActive ? "active-link": "link"} to="/register">Register</NavLink>
-            
+            </div>
         </>
     );
 }
