800254d87bed88a30ba80f397b4040f09b7c34b4
Viviancat666 <vivian9069@gmail.com>
2025-04-08 22:13:19 +0300

    add form edited

--- client/vite-project/src/components/AddMonster.jsx
+++ client/vite-project/src/components/AddMonster.jsx
@@ -1,15 +1,38 @@
-
-
 export default function AddMonster() {
-   return (
-     <>
-        <h1>Add Monster</h1>
-        <form action="">
-            <input type="text" name="name" />
-            <input type="text" name="powers" />
-            <input type="text" name="weaknesses" />
-
-        </form>
-     </>
-   );
+    return (
+        <>
+            <h1>Add Monster</h1>
+            <form className={"column form-card"} action="">
+                <div className="form-row-container">
+                    <div className="form-row-left-container">
+                        <div className="form-heading-line"></div>
+                        <label htmlFor="image">Photo</label>
+                    </div>
+                    <input className="form-input" type="text" name="image" />
+                </div>
+                <div className="form-row-container">
+                    <div className="form-row-left-container">
+                        <div className="form-heading-line"></div>
+                        <label htmlFor="name">Name</label>
+                    </div>
+                    <input className="form-input" type="text" name="name" />
+                </div>
+                <div className="form-row-container">
+                    <div className="form-row-left-container">
+                        <div className="form-heading-line"></div>
+                        <label htmlFor="powers">Powers</label>
+                    </div>
+                    <textarea className="form-input" name="powers" />
+                </div>
+                <div className="form-row-container">
+                    <div className="form-row-left-container">
+                        <div className="form-heading-line"></div>
+                        <label htmlFor="weaknesses">Weaknesses</label>
+                    </div>
+                    <textarea className="form-input" name="weaknesses" />
+                </div>
+                <button  className="form-button" type="submit">CREATE</button>
+            </form>
+        </>
+    );
 }
