c78f1fbd925fb7f9c5d12c203c1bce07051278eb
Viviancat666 <vivian9069@gmail.com>
2025-04-09 20:55:19 +0300

    home name image added

--- client/vite-project/src/components/Home.jsx
+++ client/vite-project/src/components/Home.jsx
@@ -1,9 +1,10 @@
 import amulet from "../assets/homeBackground.png"
-
+import name from "../assets/name3.png"
 export default function Home() {
    return (
      <div  className={"home-background-amulet"} style={{backgroundImage:`url(${amulet})`}}>
-     <h1>Home</h1>
+     {/* <h1>Home</h1> */}
+     <img src={name} alt="" />
      </div>
    );
 }
